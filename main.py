d83880c608db38e37be89c6565ba827f614e42a0
firedrak <101325463+firedrak@users.noreply.github.com>
2022-03-21 12:25:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+
+from settings import *
+
+redisClient = redisCli()
+if redisClient.get_status() != 'running': first_job()
+
+import asyncio
+import template
+import aiohttp
+from bs4 import BeautifulSoup
+from multiprocessing import Process
+
+redisClient = redisCli()
+
+procersses = []
+
+redisClient.start_crawling()
+
+conn = aiohttp.TCPConnector(limit_per_host=100, limit=0, ttl_dns_cache=300)
+
+def process_page(page):
+    doc = BeautifulSoup(page['content'], 'html.parser')
+    fun_to_call = getattr(template, page['call_back'])
+    result = fun_to_call(doc)
+    if 'data' in result.keys() and len(result['data']):
+        redisClient.redis_push('data', result['data'])
+    if 'url' in result.keys() and len(result['url']):
+        for job in result['url']:
+            redisClient.redis_push('job_queue', job)
+
+async def extracting():
+    while redisClient.get_status() == 'running':
+        job_left = (redisClient.length_of_queue('job_queue'))
+        page_left = (redisClient.length_of_queue('page_queue'))
+        if page_left and job_left < 5:
+            page = redisClient.redis_pop('page_queue')
+            redisClient.incr_process_count()
+            process_page(page)
+            redisClient.dicr_process_count()
+        await asyncio.sleep(.01)
+
+async def fetching():
+
+    semaphore = asyncio.Semaphore(50)
+    session = aiohttp.ClientSession(connector=conn)
+    
+    async def push_page(job):
+        url = job['url']
+        redisClient.incr_process_count()
+        async with semaphore:
+            async with session.get(url, ssl=False) as response:
+                doc = await response.text()
+                redisClient.redis_push('page_queue', {'content':doc, 'call_back':job['call_back']})
+                redisClient.dicr_process_count() 
+
+    while redisClient.get_status() == 'running':
+        job_left = (redisClient.length_of_queue('job_queue'))
+        page_left = (redisClient.length_of_queue('page_queue'))
+        if job_left and page_left < 10:
+            asyncio.create_task(push_page(redisClient.redis_pop('job_queue')))
+        await asyncio.sleep(0.01)
+
+    # await session.close()  
+
+async def main():
+    
+    fetching_routine = asyncio.ensure_future(fetching())
+    extracting_routine = asyncio.ensure_future(extracting())
+    
+    while redisClient.get_status() == 'running':
+
+        await asyncio.sleep(1)
+        if redisClient.length_of_queue('job_queue') == 0 and redisClient.length_of_queue('page_queue') == 0:
+            if redisClient.get_process_count() == '0':
+                redisClient.stop_crawling()
+
+        job_len = redisClient.length_of_queue('job_queue')
+        page_len = redisClient.length_of_queue('page_queue')
+        data_len = redisClient.length_of_queue('data')
+        state = redisClient.get_status()
+        count = redisClient.get_process_count()
+        print(f'job_len:{job_len} page_len:{page_len} count:{count} data_len:{data_len} state:{state}')
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+
+
+    # import pdb; pdb.set_trace()
---
+++ settings.py
@@ -0,0 +1,56 @@
+import requests
+import redis
+import json, sys
+# from 'https://github.com/firedrak/spider.git' import template
+
+OUT_PUT_FILE_NAME = 'dataCollected.csv'
+
+class redisCli:
+
+    redis_host = 'localhost'
+    redis_port = 6379
+
+    REDIS_CLI = redis.StrictRedis(
+        host=redis_host, port=redis_port, decode_responses=True)
+
+    def get_status(self):
+        return self.REDIS_CLI.get('state')
+
+    def start_crawling(self):
+        self.REDIS_CLI.set('state', 'running')
+
+    def stop_crawling(self):
+        self.REDIS_CLI.set('state', 'stopped')
+
+    def length_of_queue(self, key):
+        return self.REDIS_CLI.llen(key)
+
+    def redis_push(self, key, value):
+        self.REDIS_CLI.lpush(key, json.dumps(value))
+
+    def redis_pop(self, key):
+        return json.loads(self.REDIS_CLI.lpop(key))
+
+    def incr_process_count(self):
+        self.REDIS_CLI.incr('process')
+
+    def dicr_process_count(self):
+        self.REDIS_CLI.decr('process')
+
+    def get_process_count(self):
+        return self.REDIS_CLI.get('process')
+
+
+args = sys.argv[1:]
+if args:
+    url = args[0]
+    doc = (requests.get(url).text)
+
+def first_job():
+    with open('template.py', 'w') as f:
+        f.write(doc)
+
+    import template
+    
+    int_job = {'url' : template.STARTING_URL, 'call_back' : 'pars'}
+    redisCli().redis_push('job_queue', int_job)
